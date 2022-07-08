569107a69810808ca9ac865db3830230d3a5efa9
Bansishingala <shingalabansi8@gmail.com>
2022-07-08 15:26:40 +0530

    Card

--- src/Container/Appointment/List_apt.js
+++ src/Container/Appointment/List_apt.js
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import {
+    Card, CardImg, CardText, CardBody,
+    CardTitle, CardSubtitle, Button
+} from 'reactstrap';
 
 function List_apt(props) {
+    const [ Data , setData] = useState([])
+
     return (
-        <div>
-                <h2> List </h2>
+        <div>{
+            Data.map((o, l) => {
+                return (
+                    <Card className='col-3 mt-5 ms-5 me-5 mb-5 border-dark  '>
+                        <CardBody>
+                            <CardTitle>Card title</CardTitle>
+                            <CardSubtitle>Card subtitle</CardSubtitle>
+                            <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
+                            <Button className=' btn-dark'>Button</Button>
+                        </CardBody>
+                    </Card>
+                )
+            })
+
+        }
+
         </div>
     );
 }
